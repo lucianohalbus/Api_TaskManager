447264ae337c8e91287992907cbf82f172a92094
lucianohalbus <luciano@halbus.com>
2025-08-19 10:36:49 -0300

    Programa.cs updates

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -3,11 +3,9 @@ namespace Api_TaskManager.Models;
 public class TaskItem
 {
     public int Id { get; set; }
-
     public required string Title { get; set; }  
     public string Description { get; set; } = string.Empty; 
     public bool Completed { get; set; }
-
     public int UserId { get; set; }
     public User User { get; set; } = null!; 
 }
