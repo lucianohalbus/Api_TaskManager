a66e8e6a9046f3943ae4cba2778a621dee6941b4
lucianohalbus <luciano@halbus.com>
2025-08-18 23:42:22 -0300

    Adding User e TaskItem Models

--- obj/Debug/net9.0/Api_TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api_TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b59b2070bee0851c864fdcff9b53cd56aaadb3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cc62ae5cdbe50a8d9d01365f0a2ca762df953f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
