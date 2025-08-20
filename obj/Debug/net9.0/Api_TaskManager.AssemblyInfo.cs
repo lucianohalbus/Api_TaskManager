ee35959c2113fa7e744326b67f8473df39b8b980
lucianohalbus <luciano@halbus.com>
2025-08-20 10:48:36 -0300

    Install BCrypt.Net-Next to use jwt

--- obj/Debug/net9.0/Api_TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api_TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625aeea442eadf55ccd986b3bbb02e4b7ef136d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8283a1dec6d3c726594f49262f2dafe13beec819")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
